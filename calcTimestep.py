40c2f9dc614597d9b4b117819c8e0d0919c5cf47
cibinjoseph <cibinjoseph92@gmail.com>
2019-01-25 17:27:53 +0530

    Add display of results

--- calcTimestep.py
+++ calcTimestep.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 import tkinter 
+import math
 
 allFont = ('Arial',12)
 
@@ -12,11 +15,11 @@ rpmEntry = tkinter.Entry(window, width=10, font=allFont)
 rpmEntry.insert(0,'600')
 rpmEntry.grid(row=0, column=1)
 
-degText = tkinter.Label(window, text='Deg / timestep', font=allFont, padx=10, pady=5)
-degText.grid(row=1, sticky='w')
-degEntry = tkinter.Entry(window, width=10, font=allFont)
-degEntry.insert(0,'5')
-degEntry.grid(row=1, column=1)
+dDegText = tkinter.Label(window, text='Deg / timestep', font=allFont, padx=10, pady=5)
+dDegText.grid(row=1, sticky='w')
+dDegEntry = tkinter.Entry(window, width=10, font=allFont)
+dDegEntry.insert(0,'5')
+dDegEntry.grid(row=1, column=1)
 
 nrevText = tkinter.Label(window, text='Revolutions', font=allFont, padx=10, pady=5)
 nrevText.grid(column=0, row=2, sticky='w')
@@ -24,9 +27,39 @@ nrevEntry = tkinter.Entry(window, width=10, font=allFont)
 nrevEntry.insert(0,'40')
 nrevEntry.grid(row=2, column=1)
 
+
 def clicked():
-    res = float(rpmEntry.get()) + float(degEntry.get())
-    omegaText.configure(text=res)
+    if not rpmEntry.get():
+        rpm = 0.0
+    else:
+        rpm = float(rpmEntry.get())
+
+    if not dDegEntry.get():
+        dDeg = 0.0
+    else:
+        dDeg = float(dDegEntry.get())
+
+    if not nrevEntry.get():
+        nrev = 0.0;
+    else:
+        nrev = float(nrevEntry.get())
+
+    omegaRPS = 2.0*math.pi*rpm/60.0;
+    timestep = (2.0*dDeg*math.pi)/(omegaRPS*180.0)
+    ntimesteps = nrev*360.0/dDeg
+
+    omegaEntry.config(state='normal')
+    omegaEntry.delete(0,'end')
+    omegaEntry.insert(0,str(omegaRPS))
+
+    timestepEntry.config(state='normal')
+    timestepEntry.delete(0,'end')
+    timestepEntry.insert(0,str(timestep))
+
+    ntimestepsEntry.config(state='normal')
+    ntimestepsEntry.delete(0,'end')
+    ntimestepsEntry.insert(0,str(ntimesteps))
+
 
 btn = tkinter.Button(window, text='Calculate', command=clicked, font=allFont)
 btn.grid(row=3, column=1)
